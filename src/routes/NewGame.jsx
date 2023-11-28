a592ae35f1fafa2b22f3fd579a96642b0daa8507
georg-popp <georg.popp@outlook.com>
2023-11-28 15:42:13 +0100

    Add randomUUID

--- src/routes/NewGame.jsx
+++ src/routes/NewGame.jsx
@@ -1,8 +1,12 @@
+import ShortUniqueId from 'short-unique-id';
+
 const NewGame = () => {
+    const { randomUUID } = new ShortUniqueId({ length: 8 });
+
     return (
         <div>
             <h1 className='text-3xl'>New Game:</h1>
-            <h2>ID: </h2>
+            <h2>ID: {randomUUID()}</h2>
         </div>
     );
 };
