2858d46058d9ffc94c2a2afed9bf3804b767553b
g-popp <Georg.Popp@outlook.com>
2023-11-30 20:48:06 +0100

    Stylings

--- src/components/ChoosePlayer.jsx
+++ src/components/ChoosePlayer.jsx
@@ -8,14 +8,12 @@ const ChoosePlayer = () => {
         <div className=' grid'>
             <div className='grid grid-cols-2 gap-4'>
                 {players.map(player => (
-                    <div
+                    <button
                         key={player.id}
-                        className={`bg-[${player.backgroundColor}] rounded-lg shadow-xl py-4 px-10`}
+                        className={`bg-[${player.backgroundColor}] rounded-lg shadow-xl py-4 px-10 text-2xl text-center text-black focus:bg-opacity-50 focus:text-white focus:outline-none`}
                     >
-                        <h2 className='text-2xl text-center text-black'>
-                            {player.name}
-                        </h2>
-                    </div>
+                        {player.name}
+                    </button>
                 ))}
             </div>
         </div>
--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
 const QuestionCard = () => {
+    const headingColors = ['#FF9B71', '#1B998B', '#FFFD82', '#E84855'];
+    const randomColor =
+        headingColors[Math.floor(Math.random() * headingColors.length)];
+    const [color] = useState(randomColor);
     return (
-        <div className='w-full border border-black px-12 py-24 bg-[#FFFD82] rounded-lg shadow-xl'>
+        <div
+            className={`w-full border border-black px-12 py-24 bg-[${color}] rounded-lg shadow-xl`}
+        >
             <h2 className='text-2xl text-center text-black'>
                 Which player is most likely to drive drunk?
             </h2>
