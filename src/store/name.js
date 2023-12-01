3d6e60eff25beb264cba40b703ec2aba47aecca1
g-popp <Georg.Popp@outlook.com>
2023-12-01 13:42:07 +0100

    use localStorage for name

--- src/store/name.js
+++ src/store/name.js
@@ -1,8 +1,9 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 import ShortUniqueId from 'short-unique-id';
 
 const { randomUUID } = new ShortUniqueId({ length: 4 });
 
-export const nameAtom = atom('');
+export const nameAtom = atomWithStorage('');
 
 export const idAtom = atom(randomUUID());
