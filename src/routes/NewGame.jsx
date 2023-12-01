1fdd93f730cab9eef618381a4615cbcc361ee2d9
g-popp <Georg.Popp@outlook.com>
2023-12-01 13:42:47 +0100

    create gameId in different way

--- src/routes/NewGame.jsx
+++ src/routes/NewGame.jsx
@@ -1,23 +1,31 @@
 import { useAtom } from 'jotai';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import ShortUniqueId from 'short-unique-id';
 import clipboard from '../assets/clipboard.png';
 import PlayerCard from '../components/PlayerCard';
 import { nameAtom } from '../store/name';
 import { playersAtom } from '../store/players';
 
+const { randomUUID } = new ShortUniqueId({ length: 8 });
+
 const NewGame = () => {
-    const { randomUUID } = new ShortUniqueId({ length: 8 });
+    const navigate = useNavigate();
+
+    const [gameId] = useState(randomUUID());
+
     const [name] = useAtom(nameAtom);
     const [players] = useAtom(playersAtom);
 
-    // socket.on('connect', console.log('Connected!'));
+    useEffect(() => {
+        navigate(`/newGame/${gameId}`, { replace: true });
+    }, []);
 
     return (
         <div className='flex flex-col gap-20 items-center'>
             <h1 className='text-3xl underline'>Game Lobby</h1>
             <div className='flex flex-row gap-6 items-center'>
-                <h2 className='text-4xl'>ID: {randomUUID()}</h2>
+                <h2 className='text-4xl'>ID: {gameId}</h2>
                 <div className='border border-black opacity-50 p-2 rounded-lg shadow-md hover:cursor-pointer'>
                     <img
                         src={clipboard}
