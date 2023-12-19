9fc4308b95027fb907ebe8457a72ee524ff4530f
g-popp <Georg.Popp@outlook.com>
2023-12-19 23:51:59 +0100

    [frontend| add: Join Lobby and check for it

--- src/routes/Join.jsx
+++ src/routes/Join.jsx
@@ -1,6 +1,34 @@
+import { useAtom } from 'jotai';
+import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { SocketContext } from '../context/socket';
+import { gameIdAtom } from '../store/game';
 
 const Join = () => {
+    const socket = useContext(SocketContext);
+    const [gameId, setGameId] = useAtom(gameIdAtom);
+
+    const joinLobby = e => {
+        e.preventDefault();
+
+        if (!gameId) return console.log('false');
+
+        gameId && socket && socket.emit('checkRoom', gameId);
+
+        // navigate('/join');
+    };
+
+    useEffect(() => {
+        socket &&
+            socket.on('roomExists', exists => {
+                if (exists) {
+                    console.log('Room exists');
+                } else {
+                    console.log('Room does not exist');
+                }
+            });
+    }, [socket]);
+
     return (
         <div className='flex flex-col gap-20 items-center'>
             <h1 className='text-3xl underline'>Join</h1>
@@ -9,7 +37,7 @@ const Join = () => {
                     <input
                         placeholder='...'
                         className='peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-lg font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-[#1B998B] focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
-                        onChange={() => {}}
+                        onChange={e => setGameId(e.target.value)}
                     />
                     <label className="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-lg font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-[#1B998B] after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-[#1B998B] peer-focus:after:scale-x-100 peer-focus:after:border-[#1B998B] peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                         Game ID
@@ -19,6 +47,7 @@ const Join = () => {
             <Link
                 to='/game'
                 className='bg-[#E84855] text-black text-center text-xl py-4 px-6 border border-black rounded shadow-sm shadow-black'
+                onClick={joinLobby}
             >
                 Join Lobby
             </Link>
