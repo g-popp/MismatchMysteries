78951adb31053c563717d5b18ec5cf75de60c4b4
g-popp <Georg.Popp@outlook.com>
2024-01-08 21:54:15 +0100

    [frontend] add: polyfills

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,21 @@
 import react from '@vitejs/plugin-react';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+
 import { defineConfig } from 'vite';
+
+import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [nodeModulesPolyfillPlugin()]
+        }
+    },
     plugins: [react()],
-    server: {
-        proxy: {
-            '/api': '*'
+    build: {
+        rollupOptions: {
+            plugins: [nodePolyfills()]
         }
     }
 });
