db58aa8f5c87920fdeedb6f1c16460c1d761a10f
g-popp <Georg.Popp@outlook.com>
2024-01-11 14:48:29 +0100

    [frontend] fix: setter of question

--- client/src/store/questions.js
+++ client/src/store/questions.js
@@ -1,3 +1,11 @@
 import { atom } from 'jotai';
 
-export const questionsAtom = atom();
+export const questionsAtom = atom(
+    [], // initial state
+    (get, set, update) => {
+        // handle the update asynchronously
+        Promise.resolve(update).then(newQuestions => {
+            set(questionsAtom, newQuestions);
+        });
+    }
+);
