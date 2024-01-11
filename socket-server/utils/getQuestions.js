ba2266667f518a9e9fba109a1a4eefaed70e8269
g-popp <Georg.Popp@outlook.com>
2024-01-11 16:34:55 +0100

    [server] refactor: sendQuestions per room and delete used questions

--- socket-server/utils/getQuestions.js
+++ socket-server/utils/getQuestions.js
@@ -1,35 +1,46 @@
 import fs from 'fs';
 
-let questions = [];
+let questions = {};
+let originalQuestions = [];
 
 try {
     const getQuestionsFromFile = fs.readFileSync(
         'utils/questions.json',
         'utf8'
     );
-    questions = JSON.parse(getQuestionsFromFile);
+    originalQuestions = JSON.parse(getQuestionsFromFile);
 } catch (error) {
     console.log(error);
 }
 
-const getRandomQuestion = () => {
+const getRandomQuestion = roomId => {
+    // If this room doesn't have its own set of questions yet, create it
+    if (!questions[roomId]) {
+        questions[roomId] = [...originalQuestions];
+    }
+
     const randomQuestion =
-        questions[Math.floor(Math.random() * questions.length)];
+        questions[roomId][Math.floor(Math.random() * questions[roomId].length)];
+
+    console.log('randomQuestion', randomQuestion);
 
     // delete selected question from array
-    const index = questions.indexOf(randomQuestion);
+    const index = questions[roomId].indexOf(randomQuestion);
     if (index > -1) {
-        questions.splice(index, 1);
+        questions[roomId].splice(index, 1);
     }
 
     return randomQuestion;
 };
 
-const getQuestions = () => {
-    const normalQuestion = getRandomQuestion();
-    const imposterQuestion = getRandomQuestion();
+const getQuestions = roomId => {
+    const normalQuestion = getRandomQuestion(roomId);
+    const imposterQuestion = getRandomQuestion(roomId);
 
-    return [normalQuestion, imposterQuestion];
+    return {
+        normalQuestion: normalQuestion,
+        imposterQuestion: imposterQuestion
+    };
 };
 
 export default getQuestions;
