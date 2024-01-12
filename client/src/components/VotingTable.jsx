c94db4e7cb02171db2dbcc2dc1226aad469e47db
g-popp <Georg.Popp@outlook.com>
2024-01-12 17:16:57 +0100

    [frontend] add: VotingTableList

--- client/src/components/VotingTable.jsx
+++ client/src/components/VotingTable.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { colors } from '../constants/colors';
+import VotingTableList from './ui/VotingTableList';
 
 const VotingTable = ({ playerChoices }) => {
     const commonStyles = 'border-2 border-black px-10 py-5 text-center';
@@ -17,24 +17,7 @@ const VotingTable = ({ playerChoices }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {playerChoices.map((choice, index) => {
-                            return (
-                                <tr
-                                    key={choice.chooser.id}
-                                    style={{
-                                        backgroundColor:
-                                            colors[`rowColor-${index % 4}`]
-                                    }}
-                                >
-                                    <td className='text-center text-black border-r-2 border-b-2 border-l-2 border-black font-bold'>
-                                        {choice.chooser.name}
-                                    </td>
-                                    <td className='text-center text-black border-b-2 border-r-2 border-black'>
-                                        {choice.chosen.name}
-                                    </td>
-                                </tr>
-                            );
-                        })}
+                        <VotingTableList playerChoices={playerChoices} />
                     </tbody>
                 </table>
             </div>
