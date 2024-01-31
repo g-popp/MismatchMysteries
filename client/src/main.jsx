a98073b1d1433e9a1e702b11866f4f5068271dc5
g-popp <Georg.Popp@outlook.com>
2024-01-31 18:46:55 +0100

    [frontend] feature: prepare Error Boundary

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
@@ -54,8 +55,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <SocketProvider>
-            <RouterProvider router={router} />
-        </SocketProvider>
+        <ErrorBoundary fallback={<div>Loading...</div>}>
+            <SocketProvider>
+                <RouterProvider router={router} />
+            </SocketProvider>
+        </ErrorBoundary>
     </React.StrictMode>
 );
