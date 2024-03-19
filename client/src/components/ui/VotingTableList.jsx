ea0bf2375147bc2c3bc2a5fbcb1293f0cf14d15e
g-popp <Georg.Popp@outlook.com>
2024-03-19 17:52:54 +0100

    [frontend] refactor: VotingTableList component to group player choices

--- client/src/components/ui/VotingTableList.jsx
+++ client/src/components/ui/VotingTableList.jsx
@@ -1,24 +1,40 @@
 /* eslint-disable react/prop-types */
 import { colors } from '../../constants/colors';
 
-const VotingTableList = ({ playerChoices }) => {
-    console.log(playerChoices);
+const groupPlayerChoices = playerChoices => {
+    return playerChoices.reduce((bundle, choice) => {
+        const existingBundle = bundle.find(
+            bundleItem => bundleItem[0].choice.name === choice.choice.name
+        );
+        if (existingBundle) {
+            existingBundle.push(choice);
+        } else {
+            bundle.push([choice]);
+        }
+        return bundle;
+    }, []);
+};
 
-    return playerChoices.map((choice, index) => (
-        <tr
-            key={index}
-            style={{
-                backgroundColor: colors[`rowColor-${index % 4}`]
-            }}
-        >
-            <td className='text-center text-black border-r-2 border-b-2 border-l-2 border-black font-bold'>
-                {choice.chooser.name}
-            </td>
-            <td className='text-center text-black border-b-2 border-r-2 border-black'>
-                {choice.choice.name}
-            </td>
-        </tr>
-    ));
+const VotingTableList = ({ playerChoices }) => {
+    const playerBundle = groupPlayerChoices(playerChoices);
+
+    return playerBundle.map((bundle, colorIndex) => {
+        return bundle.map((player, playerIndex) => (
+            <tr
+                key={playerIndex}
+                style={{
+                    backgroundColor: colors[`rowColor-${colorIndex % 4}`]
+                }}
+            >
+                <td className='text-center text-black border-r-2 border-b-2 border-l-2 border-black font-bold'>
+                    {player.chooser.name}
+                </td>
+                <td className='text-center text-black border-b-2 border-r-2 border-black'>
+                    {player.choice.name}
+                </td>
+            </tr>
+        ));
+    });
 };
 
 export default VotingTableList;
