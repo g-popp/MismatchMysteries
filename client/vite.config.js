8b44aaeca78f077945a7c6fd462b0a13e0034993
g-popp <Georg.Popp@outlook.com>
2024-05-31 18:49:21 +0200

    [frontend] remove: nodePolyfills

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,8 @@
 import react from '@vitejs/plugin-react';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 import { defineConfig } from 'vite';
 
-import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        esbuildOptions: {
-            plugins: [nodeModulesPolyfillPlugin()]
-        }
-    },
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            plugins: [nodePolyfills()]
-        }
-    }
+    plugins: [react()]
 });
