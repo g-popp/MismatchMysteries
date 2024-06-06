f3398654acb94282e878dd95d00f02d97fd1aa40
g-popp <Georg.Popp@outlook.com>
2024-06-06 16:32:46 +0200

    [frontend] refactor: input and background

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -102,24 +102,27 @@ const Home = () => {
 
     return (
         <div className='flex flex-col p-8 items-center gap-8'>
-            <h1 className='text-6xl underline py-20 text-center'>
+            <h1 className='text-6xl text-black underline py-20 text-center'>
                 Friends Don&apos;t Lie
             </h1>
-            <h2 className='text-xl mb-8'>Enter a Name to start a game!</h2>
+            <h2 className='text-xl text-black mb-8'>
+                Enter a Name to start a game!
+            </h2>
             <div>
                 <div className='relative h-12 w-full min-w-[200px]'>
                     <input
                         placeholder='...'
                         value={ownPlayer.name}
                         form='username'
-                        className='peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-[#1B998B] focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
+                        type='text'
+                        className='form-textbox peer h-full w-full border-b border-black bg-transparent pt-4 pb-1.5 text-sm font-normal text-black outline outline-0 transition-all placeholder-shown:border-black focus:border-[#1B998B] focus:outline-0 disabled:border-0 disabled:bg-black'
                         onChange={e =>
                             setOwnPlayer({ ...ownPlayer, name: e.target.value })
                         }
                     />
                     <label
                         name='username'
-                        className="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-sm font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-[#1B998B] after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-[#1B998B] peer-focus:after:scale-x-100 peer-focus:after:border-[#1B998B] peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
+                        className="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-sm font-normal leading-tight text-black transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-[#1B998B] after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-black peer-focus:text-sm peer-focus:leading-tight peer-focus:text-[#1B998B] peer-focus:after:scale-x-100 peer-focus:after:border-[#1B998B] peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-black"
                     >
                         Name
                     </label>
