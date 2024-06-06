de245f9a3eb9ac65a8f1f0c737ab35c7dd048f40
g-popp <Georg.Popp@outlook.com>
2024-06-06 16:32:46 +0200

    [frontend] refactor: input and background

--- client/src/routes/Join.jsx
+++ client/src/routes/Join.jsx
@@ -82,11 +82,12 @@ const Join = () => {
                 <div className='relative h-12 w-full min-w-[200px]'>
                     <input
                         placeholder='...'
-                        className='peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-lg font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-[#1B998B] focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
+                        type='text'
+                        className='form-textbox peer h-full w-full border-b border-black bg-transparent pt-4 pb-1.5 text-lg font-normal text-black outline outline-0 transition-all placeholder-shown:border-black focus:border-[#1B998B] focus:outline-0 disabled:border-0 disabled:bg-black'
                         onChange={e => setGameId(e.target.value)}
                         value={gameId}
                     />
-                    <label className="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-lg font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-[#1B998B] after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-[#1B998B] peer-focus:after:scale-x-100 peer-focus:after:border-[#1B998B] peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-lg font-normal leading-tight text-black transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-[#1B998B] after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-black peer-focus:text-sm peer-focus:leading-tight peer-focus:text-[#1B998B] peer-focus:after:scale-x-100 peer-focus:after:border-[#1B998B] peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-black">
                         Game ID
                     </label>
                 </div>
