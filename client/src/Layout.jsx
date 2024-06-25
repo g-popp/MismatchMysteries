7628348ac2e56fb7f41ec5d866d602a8743b3f17
g-popp <Georg.Popp@outlook.com>
2024-06-25 23:24:11 +0200

    [frontend] chore: change headline color + cleanup

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    const headingColors = ['#FF9B71', '#1B998B', '#FFFD82', '#E84855'];
-    const randomColor =
-        headingColors[Math.floor(Math.random() * headingColors.length)];
-    const [color] = useState(randomColor);
-
     return (
         <div className='flex flex-col p-12 items-center'>
             <Link to='/'>
-                <h1
-                    className='text-3xl underline basis-4/5 text-center mb-12'
-                    style={{ color: color }}
-                >
+                <h1 className='text-3xl underline basis-4/5 text-center mb-12 text-teal-600'>
                     Friends Don&apos;t Lie
                 </h1>
             </Link>
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,76 +1,22 @@
-import { useAtom } from 'jotai';
-import { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button';
-import Toast from '../components/Toast';
-import { SocketContext } from '../context/socket';
-import { useToast } from '../hooks/useToast';
-import { playerAtom } from '../store/players';
-import { roomAtom } from '../store/room';
+import { Link } from 'react-router-dom';
 
 import Logo from '../assets/logo.svg';
 
 const Home = () => {
-    const socket = useContext(SocketContext);
-
-    const navigate = useNavigate();
-    const [ownPlayer, setOwnPlayer] = useAtom(playerAtom);
-    const [room, setRoom] = useAtom(roomAtom);
-
-    const [showToast, toastType, toastMessage, hideToast] = useToast();
-
-    const [playerSet, setPlayerSet] = useState(false);
-
-    const handleNewGame = e => {
-        e.preventDefault();
-
-        socket.emit('createLobby', ownPlayer.name);
-    };
-
-    useEffect(() => {
-        if (socket) {
-            socket.on('lobbyCreated', room => {
-                setRoom(room);
-
-                socket.on('playerInfo', player => {
-                    setOwnPlayer(player);
-                    setPlayerSet(true);
-                });
-            });
-
-            socket.on('error', message => {
-                console.error(message);
-            });
-
-            return () => {
-                socket.off('lobbyCreated');
-                socket.off('playerInfo');
-                socket.off('error');
-            };
-        }
-    }, [socket]);
-
-    useEffect(() => {
-        if (playerSet && room.id) {
-            navigate(`/join`);
-        }
-    }, [playerSet, room]);
-
     return (
-        <div className='flex flex-col p-8 items-center gap-40'>
+        <div className='flex flex-col p-8 mt-8 items-center gap-40'>
             <h1 className='text-6xl text-black underline text-center'>
                 Friends Don&apos;t Lie
             </h1>
             <img src={Logo} className='w-10/12 lg:w-1/3' />
-            <Button color='#1B998B' handler={e => handleNewGame(e)}>
+            <Link
+                to='/join'
+                className={
+                    'text-black text-center text-xl py-4 px-6 border border-black rounded shadow-sm shadow-black bg-teal-600'
+                }
+            >
                 Play Now
-            </Button>
-            <Toast
-                message={toastMessage}
-                type={toastType}
-                show={showToast}
-                onClose={hideToast}
-            />
+            </Link>
         </div>
     );
 };
