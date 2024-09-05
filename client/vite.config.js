74f2d89e5e123198dcf3d688c0fbced778e0f507
ext.Georg Popp <Georg.Popp@outlook.com>
2024-09-05 23:22:31 +0200

    [frontend] fix: resolve icon issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            external: ['@phosphor-icons/react']
-        }
-    }
+    plugins: [react()]
+    // build: {
+    //     rollupOptions: {
+    //         external: ['@phosphor-icons/react']
+    //     }
+    // }
 });
