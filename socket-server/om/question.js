ab50e8a5f5a0980c08f31df5345e248329d73bad
ext.Georg Popp <Georg.Popp@outlook.com>
2024-09-19 03:05:42 +0200

    [server] feat: extend question schema with createdAt, deletedAt, and forAdults properties

--- socket-server/om/question.js
+++ socket-server/om/question.js
@@ -3,7 +3,10 @@ import redis from './client.js';
 
 const questionSchema = new Schema('question', {
     text: { type: 'string' },
-    active: { type: 'boolean' }
+    active: { type: 'boolean' },
+    createdAt: { type: 'date', sortable: true },
+    deletedAt: { type: 'date' },
+    forAdults: { type: 'boolean' }
 });
 
 export const questionRepository = new Repository(questionSchema, redis);
